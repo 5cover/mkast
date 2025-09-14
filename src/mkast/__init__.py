9b4478dd02d225bb09be8102bbe6a15e41b5e9f5
Scover <>
2025-09-14 20:02:42 +0200

    added __all__

--- src/mkast/__init__.py
+++ src/mkast/__init__.py
@@ -10,3 +10,5 @@ from .cfg import read_config, Config, Modifier, ModifierKey
 from .mkast import generate_ast, register_emitter, unregister_emitter, get_emitter
 # emitter type
 from .domain import Emitter
+
+__all__ = [load_input.__name__, read_config.__name__, Config.__name__, Modifier.__name__, ModifierKey.__name__,generate_ast.__name__, register_emitter.__name__, unregister_emitter.__name__, get_emitter.__name__, Emitter.__name__, '__version__']
